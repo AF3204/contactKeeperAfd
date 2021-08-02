d42457d3a425091d36baa9ace2262dc8b1415320
Faizal Dar-Sandur <afaizal1987@gmail.com>
2021-08-02 14:18:57 +0800

    ADDED: Contact CRUD API

--- routes/contacts.js
+++ routes/contacts.js
@@ -12,32 +12,145 @@ const auth = require('../middleware/auth.js')
 // @desc        Get user contacts
 // @access      Private
 // @param       user
-router.get('/',(req, res)=>{
-    res.send('Get all contacts')
+router.get('/',auth,async (req, res)=>{
+    try{
+        const contacts = await Contact.find({user: req.user.id}).sort({date: -1})
+        res.json(contacts)
+    }catch(err){
+        console.log(err.message)
+        res.status(500).json({
+            msg: ' Server Error - Unable to find contacts'
+        })
+    }
 })
 
 // @route       POST api/auth
 // @desc        Add new contact
 // @access      Private
 // @param       NULL
-router.post('/',(req, res)=>{
-    res.send('Add new user')
+router.post('/',
+    auth,
+    [
+        check('name', 'Name is required')
+            .not()
+            .isEmpty(),
+    ],
+    async (req, res)=>{
+        // 20210802: Validating the results
+        const errors = validationResult(req)
+        
+        // 20210802: Check for errors before continuing
+        if(!errors.isEmpty()){
+            return res.status(400).json({
+                msg:errors.array()
+            })
+        }
+
+        // 20210802: Initialising the constants
+        const {
+            name,
+            email,
+            phone,
+            type
+        } = req.body
+
+        try{
+            // 20210802 - user:req.user.id -> save a new contact with reference to this ID
+            const newContact = new Contact({
+                name,
+                email,
+                phone,
+                type,
+                user:req.user.id
+            })
+
+            // 20210802 - If not needed to send the saved contacts, just await is enough
+            const contact = await newContact.save();
+
+            res.json(contact)
+        }catch(err){
+            console.log(err.message);
+            res.status(500).json({
+                msg:'Unable to insert new contacts - Server Error'
+            })
+        }
+
 })
 
 // @route       PUT api/auth
 // @desc        Update contact
 // @access      Private
 // @param       NULL
-router.put('/:id',(req, res)=>{
-    res.send('Update contact')
+router.put('/:id',auth, async(req, res)=>{
+    // res.send('Update contact')
+    const {
+        name,
+        email,
+        phone,
+        type
+    } = req.body
+
+    const contactFields = {};
+
+    if(name) contactFields.name = name;
+    if(email) contactFields.email = email;
+    if(phone) contactFields.phone = phone;
+    if(type) contactFields.type = type;
+
+    try{
+        //  20210802 - We get the param from the link
+        let contact = await Contact.findById(req.params.id)
+
+        if(!contact) return res.status(404).json({ msg:'Contact not found!'})
+
+        if(contact.user.toString() !== req.user.id){
+            return res.status(401).json({ msg: 'Not authorized to change the details'})
+        }
+
+        contact = await Contact.findByIdAndUpdate(req.params.id,
+                                                    {$set: contactFields},
+                                                    {new: true, useFindAndModify: false});
+        res.status(200).json({
+            status:'Success',
+            msg:contact
+        })
+
+    }catch(err){
+        console.log(err.object);
+        res.status(500).json({
+            msg:'Unable to insert new contacts - Server Error'
+        })
+    }
 })
 
 // @route       DELETE api/auth
 // @desc        Add new contact
 // @access      Private
 // @param       NULL
-router.delete('/:id',(req, res)=>{
-    res.send('Delete contact')
+router.delete('/:id',auth,async (req, res)=>{
+    try{
+        //  20210802 - We get the param from the link
+        let contact = await Contact.findById(req.params.id)
+
+        if(!contact) return res.status(404).json({ msg:'Contact not found!'})
+
+        if(contact.user.toString() !== req.user.id){
+            return res.status(401).json({ msg: 'Not authorized to change the details'})
+        }
+
+        await Contact.findByIdAndRemove(req.params.id,{useFindAndModify:false});
+        
+        res.status(200).json({
+            status:'Success',
+            msg:`ID ${contact.name} has been removed`
+        })
+
+    }catch(err){
+        console.log(err);
+        res.status(500).json({
+            msg:'Unable to remove contacts - Server Error'
+        })
+    }
 })
 
 module.exports = router;
