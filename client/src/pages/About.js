275e220229dcfd0b9b8887fae65df754d221ae6c
Faizal Dar-Sandur <afaizal1987@gmail.com>
2021-08-03 17:52:26 +0800

    MODIFYING: Cleanup, Navbar & Router Setup

---
+++ client/src/pages/About.js
@@ -0,0 +1,12 @@
+import React,{Fragment} from "react"
+const Home =()=>{
+    return(
+        <div>
+            <Fragment>
+                <h1>About</h1>
+                <p>Building the full application</p>
+                
+            </Fragment>
+        </div>
+    )
+}
