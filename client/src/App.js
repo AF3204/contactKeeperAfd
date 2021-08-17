9e13032334aad5ea8865c2605046ec8c9e4bf621
Faizal Dar-Sandur <afaizal1987@gmail.com>
2021-08-17 15:22:46 +0800

    ADDED:  Register & Login Forms

--- client/src/App.js
+++ client/src/App.js
@@ -7,9 +7,12 @@ import About from './components/pages/About'
 
 // Contact
 import ContactState from './context/contact/ContactState';
-
-// Auth
+// 20210816 - Auth
 import AuthState from './context/auth/AuthState';
+// 20210817 - Register & Login-> This is a link
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+
 
 const App=()=>{
   /**
@@ -25,6 +28,8 @@ const App=()=>{
         <Switch>
           <Route exact path='/' component={Home}/>
           <Route exact path='/about' component={About}/>
+          <Route exact path='/register' component={Register}/>
+          <Route exact path='/login' component={Login}/>
         </Switch>
           
       </Router>
